34bdebb83df0df7a405f46343b21e67fbd753c45
Daniel Truong <ddtruong06@gmail.com>
2020-01-18 07:58:30 -0800

    Moves recognizer logic into the app.js

--- blindhelp/src/components/Button/index.jsx
+++ blindhelp/src/components/Button/index.jsx
@@ -1,64 +1,9 @@
 import React from 'react';
 import './style.css';
 
-// Represents the speech recognition object.
-const recognition = new window.webkitSpeechRecognition();
-recognition.continuous = true;
-recognition.interimResults = true;
-
-// Represents the current status of whether the microphone is currently listening to sound.
-let recognizing = false;
-
-recognition.onstart = () => {
-    recognizing = true;
-}
-
-recognition.onend = () => {
-    recognizing = false;
-}
-
-recognition.onresult = (event) => {
-    for (var i = event.resultIndex; i < event.results.length; ++i) {
-        const word = event.results[i][0].transcript;
-        console.log(word);
-        if (word.indexOf(' ') < 0) {
-            // const request = new Request('/send', {method: 'POST', body: `{"word": "oof"}`});
-            const request = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "word": word
-                })
-            };
-            const url = '/sendtodragon';
-            fetch(url, request)
-                .then(response => response.json())
-                .then((data) => {
-                    if (!data["wordSent"]) {
-                        recognition.stop();
-                    }
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        };
-    }
-}
-
-const makeRecognition = (event) => {
-    console.log("button pressed");
-    if (recognizing) {
-        recognition.stop();
-    } else {
-        recognition.start();
-    }
-  }
-
-const Button = () => {
+const Button = (props) => {
     return (
-        <div onClick={makeRecognition} className="button">
+        <div onClick={props.recogFunc} className="button">
             <p>Microphone</p>
         </div>
     )
